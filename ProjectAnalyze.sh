d3fc945c57af140ae6f87df5159d369da284c7a2
Tianyi Zhang <zhangt73@mills.cas.mcmaster.ca>
2018-02-23 22:16:46 -0500

    working on the putsTODO feature

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -6,3 +6,10 @@ upToDate() {
 uncommittedChanges() {
   git diff > changes.log
 }
+
+todos() {
+  grep -r --exclude={} "#TODO" > todo.log
+  # reference from https://gist.github.com/a1phanumeric/5346170 
+  # To find out how to use exclude flag with grep command 
+}
+:
