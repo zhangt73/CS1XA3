0fc5ab88828e7d0a8dee4093099f2a2478296a30
Tianyi Zhang <zhangt73@mills.cas.mcmaster.ca>
2018-02-23 23:15:50 -0500

    add basic control flow

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
-upToDate() {
+clear
+echo "Welcome To Project Analyzer. You are provided with some tools to analyze your current project. Enter the corresponding command to conduct each specific task. If you are not sure where to start with, try type HELP and help will be on your way :) "
+
+ACTION=NOTSET
+while [ "$ACTION" != "quit" ] 
+do 
+    read -p "Please enter the command in order to conduct analysis: " ACTION
+    if [ "$ACTION" = "HELP" ] 
+    then 
+         helpMenu
+    elif [ "$ACTION" = "state" ]  
+    then 
+         upToDate
+   elif [ "$ACTION" = "change" ]
+    then
+         uncommittedChanges
+   elif [ "$ACTION" = "change" ]
+    then
+         uncommittedChanges
+   elif [ "$ACTION" = "todo" ]
+    then
+         todos
+   elif [ "$ACTION" = "error" ]
+    then 
+         directErrors
+   else
+         echo "Invalid Command. Please try another time"
+done 
+
+          
+
+upToDate() { 
   git status 
 }
 
