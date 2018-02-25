0c09fb88cec590358a071f4a40801e86dbe78cfc
Tianyi Zhang <zhangt73@mills.cas.mcmaster.ca>
2018-02-25 11:00:19 -0500

    ready to add another feature

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -11,22 +11,40 @@ upToDate() {
 uncommittedChanges() {
   git diff > changes.log 
  # Check the diff using git diff command and direct the out put to changes.log
+  echo "Successful! Check out changes.log to see all uncommittedChanges"
 }
 
 # Question 3: Put each line from every file of the project with tag TODO in todo.log
 
 todos() {
-  grep -r --exclude={todo.log,ProjectAnalyze.sh} "#TODO" > todo.log
+  grep -r --exclude={todo.log,ProjectAnalyze.sh,changes.log}  "#TODO" > todo.log
   # Use grep command to filter the lines with TODO tags.
   # Use --exclude tag to avoid duplication 
   # reference from https://gist.github.com/a1phanumeric/5346170 
-  # For how to use exclude flag with grep command 
+  # For how to use exclude flag with grep command
+  echo "Successful! Check out todo.log to see all TODO tasks" 
 }
 
 # Question 4: Direct all syntax error results in Haskell files to error.log:
 directErrors() {
   ghc -fno-code "*.hs" &> error.log
   # Use &> to direct errors into the target
+  echo "Successful! Check out error.log to see all errors occured in Haskell files"
+}
+
+# Extra Feature: display a list of biggest files/dirs in the current dir
+biggest() {
+  read -p "Please indicate how many files/dirs do you want to be in the list by typing a single number: " NUM
+  du -hs * | sort -rh | head -$NUM
+
+} 
+helpMenu() {
+ printf "1)state ---> Check whether the local repo is up to date with remote
+ \n2)change ---> Direct all uncommitted changes
+\n3)todo ---> Put all task with TODO tages in todo.log
+\n4)error ---> Direct errors of all Haskell files to error.log
+\n5)quit --------------------To quit the program-----------------
+"
 }
 
 # General control flow of the bash script
@@ -50,6 +68,9 @@ do
     elif [ "$ACTION" = "error" ]
     then 
          directErrors
+    elif [ "$ACTION" != "list" ]
+    then
+         biggest
     elif [ "$ACTION" != "quit" ]
     then
          echo "Invalid Command. Please try another time"
