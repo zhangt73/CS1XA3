8bc73368b2664eb7bf9274b0e5ad0f766e599748
Tianyi Zhang <zhangt73@mills.cas.mcmaster.ca>
2018-03-25 15:00:41 -0400

    update ui and features

--- Assign2/main.js
+++ Assign2/main.js
@@ -1,32 +1,40 @@
+var mode
+var inputError = false
+var inputErrorMessage
+
 document.getElementById('output').style.visibility = 'hidden' 
  
 document.getElementById('input').addEventListener('input',function(e){ 
 		let gpa = e.target.value;
-		console.log(gpa)
-		document.getElementById('output').style.visibility = 'visible'
+		gpa = parseFloat(gpa)
+		if (gpa <0 || gpa >12) {
+			alert("The cumulative gpa you entered is not valid")
+		}else { 
+			update(gpa)  
+		}
+}) 
 
-    	var output1= document.getElementById('4output') 
-   	 	var output2 = document.getElementById('aplhaOutput') 
-   	 	var output3 = document.getElementById('rangeOutput') 
+var update = function(gpa) {
+	document.getElementById('output').style.visibility  = 'visible'
+	var output1= document.getElementById('4output') 
+   	var output2 = document.getElementById('aplhaOutput') 
+   	var output3 = document.getElementById('rangeOutput') 
 
-   	 	var scaleFour = convert(gpa)[0]
-   	 	var alpha = convert(gpa)[1]
-   	 	var range = convert(gpa)[2]
+   	var scaleFour = convert(gpa)[0]
+   	var alpha = convert(gpa)[1]
+   	var range = convert(gpa)[2]
 
-   	 	if(scaleFour == null || alpha == null || range == null) {
+   	if(scaleFour == null || alpha == null || range == null) {
    	 		// error
    	 	}else {
    	 		output1.innerHTML = scaleFour
     	    output2.innerHTML = alpha
     	    output3.innerHTML = range
    	 	}
-   	 	
-}) 
+}
 
-var convert = function(str) {
-	gpa = Math.floor(parseFloat(str))
-
-	// var scale4, alpha, percentage
+var convert = function(gpa) { 
+	var gpa = Math.floor(gpa)
 	switch(gpa) {
 		case 12:
 		   return [4.0, "A+", "90-100"] 
@@ -71,3 +79,81 @@ var convert = function(str) {
 		   return null
 	}
 }
+
+var alphaDict = {
+	"A+":12,
+	"A":11,
+	"A-":10,
+	"B+":9,
+	"B":8,
+	"B-":7,
+	"C+":6,
+	"C":5,
+	"C-":4,
+	"D+":3,
+	"D":2,
+	"D-":1,
+	"F":0 
+}
+parseMarks = function(mode,marks) {
+	var markArr = marks.split(" ")
+	
+	if(mode === "Numeric") {
+		var sum = 0	
+		for(var i=0;i<markArr.length;i++ ) {
+			if(isNaN(markArr[i]) || markArr[i] < 0 || markArr[i]>12)  {
+				inputError = true
+			}
+			sum += parseFloat(markArr[i]) 
+		}
+		return sum/ markArr.length
+	}
+	else if(mode === "Alpha") {
+		var sum = 0
+		for(var i=0;i<markArr.length;i++) {
+			var singleMark = alphaDict[markArr[i]] 
+			if(isNaN(singleMark)) {
+				inputError = true
+			}
+			sum += singleMark 
+		}
+		return sum/ markArr.length
+	} 
+
+}
+let options = document.querySelectorAll('.dropdown-menu a')
+options.forEach(function(option){
+	option.addEventListener('click',function(e){
+		e.preventDefault()
+		var selText = this.textContent
+		button = document.getElementById('modeButton')
+		button.textContent = selText
+		mode = selText
+	})
+}) 
+
+var submit = document.getElementById('submitButton')
+submit.addEventListener('click',function(e){
+	if(mode !== null) {
+		var submitText = document.getElementById('submitText')
+	    var markStr = submitText.value
+	    	if (markStr !==null) {
+	    		var gpaNum = parseMarks(mode, markStr)
+			if (gpaNum == null) {
+				inputError = true 
+			}else {
+		   		console.log(gpaNum)
+		   		if(inputError) {
+		   			alert("Input Error Dected")
+		   			inputError = false
+		   		}else {
+		   			update(gpaNum)
+	   		    }
+		   		
+			}
+	    }
+		
+	}
+})
+
+
